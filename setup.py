4ce04ac87d9cac41c3c1b26451620142f9a1d1e0
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-08 10:57:45 +0000

    configure setup for pypy version 2

--- setup.py
+++ setup.py
@@ -1,19 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import stat
+import sys
+from os import geteuid, listdir, mkdir, chmod
+from os.path import  expanduser, isfile, join
 from setuptools import setup
 
-setup(name='pg_chameleon',
-	version='1.0.alpha.1',
-	description='MySql to PostgreSQL replication system',
-	url='https://github.com/the4thdoctor/pg_chameleon',
-	author='Federico Campoli',
-	author_email='4thdoctor.gallifrey@gmail.com',
-	license='BSD License',
-	packages=['pg_chameleon'],
-	zip_safe=False,
+if geteuid() == 0:
+	cham_dir = '/usr/local/etc/pg_chameleon'
+else:
+	cham_dir = "%s/.pg_chameleon" % expanduser('~')	
+	
+if geteuid() != 0:	
+	try:
+		mkdir(cham_dir)
+		mkdir('%s/logs' % cham_dir)
+		mkdir('%s/pid' % cham_dir)
+	except:
+		pass
+	chmod(cham_dir, stat.S_IRWXU)
+
+
+sql_up_path = 'sql/upgrade'
+conn_dir = "%s/connection" % cham_dir
+sql_dir = "%s/sql" % cham_dir
+sql_up_dir = "%s/%s" % (cham_dir, sql_up_path)
+
+
+data_files = []
+conf_files = (conf_dir, ['config/config-example.yaml'])
+
+sql_src = ['sql/create_schema.sql', 'sql/drop_schema.sql']
+
+sql_upgrade = ["%s/%s" % (sql_up_path, file) for file in listdir(sql_up_path) if isfile(join(sql_up_path, file))]
+
+sql_files = (sql_dir,sql_src)
+sql_files = (sql_dir,sql_src)
+sql_up_files = (sql_up_dir,sql_upgrade)
+
+
+data_files.append(conf_files)
+data_files.append(sql_files)
+data_files.append(sql_up_files)
+setup(
+	name="pg_chameleon",
+	version="v2.0-dev.1",
+	description="MySQL to PostgreSQL replica",
+	long_description="""Pg_chameleon is a replication tool from MySQL to PostgreSQL developed in Python 3.3+
+The system relies on the mysql-replication library to pull the changes from MySQL and covert them into a jsonb object. 
+A plpgsql function decodes the jsonb and replays the changes into the PostgreSQL database.
+
+The tool requires an  initial replica setup which pulls the data from MySQL in read only mode. 
+This is done by the tool running FLUSH TABLE WITH READ LOCK;  .
+
+The tool can pull the data from a cascading replica when the MySQL slave is configured with log-slave-updates.
+""",
+	author="Federico Campoli",
+	author_email="the4thdoctor.gallifrey@gmail.com",
+	url="https://github.com/the4thdoctor/pg_chameleon/",
+	platforms=[
+		"linux"
+	],
+	classifiers=[
+		"License :: OSI Approved :: BSD License"
+	],
+	#packages=['pg_chameleon'],
+	py_modules=[
+		"pg_chameleon.__init__",
+		"pg_chameleon.lib.global_lib",
+		"pg_chameleon.lib.mysql_lib",
+		"pg_chameleon.lib.pg_lib",
+		"pg_chameleon.lib.sql_util"
+	],
+	scripts=[
+		"scripts/chameleon.py"
+	],
 	install_requires=[
-								'PyMySQL', 
-								'argparse', 
-								'mysql-replication', 
-								'psycopg2', 
-								'wsgiref', 
-								'PyYAML'
-	],)
+					'PyMySQL>=0.7.6', 
+					'argparse>=1.2.1', 
+					'mysql-replication>=0.11', 
+					'psycopg2>=2.7.0', 
+					'PyYAML>=3.11', 
+					
+	],
+	data_files = data_files, 
+	include_package_data = True
+	
+)
