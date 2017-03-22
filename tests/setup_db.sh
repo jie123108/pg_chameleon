fa71fda6ea558aa610071c9207090f61027bcea8
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-22 06:15:19 +0000

    add dynamic path for mysql setup

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+here=`dirname $0`
 psql -c "create user usr_test with password 'test';" -U postgres
 psql -c 'create database db_test with owner usr_test;' -U postgres
-mysql < setup_mysql.sql
+mysql < ${here}/setup_mysql.sql
