ec8947faf386c38459ec7fcf005d0ca78262f63a
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-22 05:46:32 +0000

    add missing install requirements

--- setup.py
+++ setup.py
@@ -86,7 +86,8 @@ pg_chameleon can pull the data from a cascading replica when the MySQL slave is 
 		'mysql-replication>=0.11', 
 		'psycopg2>=2.7.0', 
 		'PyYAML>=3.11', 
-					
+		'tabulate>=0.7.7', 
+		'daemonize>=2.4.7', 
 	],
 	data_files = data_files, 
 	include_package_data = True
