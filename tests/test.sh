6a998e246dd01e95e8d9a345f5499b1ce2cc4054
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-22 05:41:37 +0000

    add set_config in tests and move the configuration file copy in test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+chameleon.py set_config
+- cp test.yaml ~/.pg_chameleon/connection/ 
 chameleon.py create_service_schema --connfile test.yaml --connkey test
 chameleon.py add_replica --connfile test.yaml --connkey test
 chameleon.py init_replica --connfile test.yaml --connkey test
