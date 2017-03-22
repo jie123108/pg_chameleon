184f0a5c8d8ffa0e6dea42e0b694b910dadcba5f
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-22 21:35:29 +0000

    add dynamic script location to test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+here=`dirname $0`
 chameleon.py set_config
-cp test.yaml ~/.pg_chameleon/connection/ 
+cp ${here}/test.yaml ~/.pg_chameleon/connection/ 
 chameleon.py create_service_schema --connfile test.yaml --connkey test
 chameleon.py add_replica --connfile test.yaml --connkey test
 chameleon.py init_replica --connfile test.yaml --connkey test
