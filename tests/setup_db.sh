c50d640a1c7c2d129a737345389699dbf10fa353
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-23 21:59:39 +0000

    add cat error.log with sudo and replica settings

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -4,5 +4,5 @@ psql -c "create user usr_test with password 'test';" -U postgres
 psql -c 'create database db_test with owner usr_test;' -U postgres
 sudo cp -f ${here}/my.cnf /etc/mysql/my.cnf
 sudo service mysql restart
-cat /var/log/mysql/error.log
+sudo cat /var/log/mysql/error.log
 mysql -u root < ${here}/setup_mysql.sql
