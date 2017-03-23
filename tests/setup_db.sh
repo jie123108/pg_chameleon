44371d751baac8db19a1d383a0e871f4699c230f
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-23 21:29:35 +0000

    copy custom my.cnf in conf.d

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -3,6 +3,6 @@ here=`dirname $0`
 psql -c "create user usr_test with password 'test';" -U postgres
 psql -c 'create database db_test with owner usr_test;' -U postgres
 cat /etc/mysql/my.cnf
-sudo cp -f ${here}/my.cnf /etc/mysql/my.cnf
+sudo cp -f ${here}/my.cnf /etc/mysql/conf.d/test.cnf
 sudo service mysql restart
 mysql -u root < ${here}/setup_mysql.sql
