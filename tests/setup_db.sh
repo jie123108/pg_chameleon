d3bd4d77faf52eb6df5d35f423d2a06d1ca59695
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-23 21:42:45 +0000

    use absolute path for binlog

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -2,6 +2,6 @@
 here=`dirname $0`
 psql -c "create user usr_test with password 'test';" -U postgres
 psql -c 'create database db_test with owner usr_test;' -U postgres
-#sudo cp -f ${here}/my.cnf /etc/mysql/conf.d/test.cnf
+sudo cp -f ${here}/my.cnf /etc/mysql/conf.d/test.cnf
 sudo service mysql restart
 mysql -u root < ${here}/setup_mysql.sql
