f51eb42a7cb6bc852fa92628709f53bd059c60a1
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-23 21:56:17 +0000

    add log_error and cat

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -4,4 +4,5 @@ psql -c "create user usr_test with password 'test';" -U postgres
 psql -c 'create database db_test with owner usr_test;' -U postgres
 sudo cp -f ${here}/my.cnf /etc/mysql/my.cnf
 sudo service mysql restart
+cat /var/log/mysql/error.log
 mysql -u root < ${here}/setup_mysql.sql
