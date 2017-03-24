263fb9795c2730ba8f9d1fb46dd55819ade8e5a1
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-24 22:15:23 +0000

    add update expired gpg key

--- tests/install_mysql.sh
+++ tests/install_mysql.sh
@@ -8,5 +8,6 @@ sudo rm -rf /var/log/mysql
 echo mysql-apt-config mysql-apt-config/enable-repo select mysql-${MYVER} | sudo debconf-set-selections
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.1-1_all.deb 
 sudo DEBIAN_FRONTEND=noninteractive dpkg --install mysql-apt-config_0.8.1-1_all.deb 
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
 sudo apt-get update -q
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" mysql-server
