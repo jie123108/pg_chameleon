140c76e47eb89bd288baf8cf08f556b7c88455d0
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-24 07:10:26 +0000

    configuration for multiple mysql versions

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -2,7 +2,7 @@
 here=`dirname $0`
 psql -c "create user usr_test with password 'test';" -U postgres
 psql -c 'create database db_test with owner usr_test;' -U postgres
-sudo cp -f ${here}/my.cnf /etc/mysql/my.cnf
+sudo cp -f ${here}/my${MYVER}.cnf /etc/mysql/my.cnf
 sudo service mysql restart
 sudo cat /var/log/mysql/error.log
 mysql -u root < ${here}/setup_mysql.sql
