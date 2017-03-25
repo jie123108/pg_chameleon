0576dd6da4126be41354744c7f3704be69822229
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-25 07:47:31 +0000

    using trusty for mysql 5.6

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -2,11 +2,11 @@
 here=`dirname $0`
 psql -c "create user usr_test with password 'test';" -U postgres
 psql -c 'create database db_test with owner usr_test;' -U postgres
-if [ "${MYVER}" != "5.5" ]
+if [ "${MYVER}" != "5.6" ]
 then
 	${here}/install_mysql.sh
 fi
-sudo cp -f ${here}/my${MYVER}.cnf /etc/mysql/my.cnf
+sudo cp -f ${here}/my${MYVER}.cnf /etc/mysql/conf.d/my.cnf
 sudo service mysql restart
 sudo cat /var/log/mysql/error.log
 mysql -u root < ${here}/setup_mysql.sql
