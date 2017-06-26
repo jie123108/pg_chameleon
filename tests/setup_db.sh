84fedbe5b9bb0302353b0d16ef3e987be0302351
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-06-26 06:44:44 +0100

    setup download and configuration for sakila db. change tests to use sakila as schema

--- tests/setup_db.sh
+++ tests/setup_db.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 here=`dirname $0`
-psql -c "create user usr_test with password 'test';" -U postgres
-psql -c 'create database db_test with owner usr_test;' -U postgres
+psql -c "CREATE USER usr_test WITH PASSWORD 'test';" -U postgres
+psql -c 'CREATE DATABASE db_test WITH OWNER usr_test;' -U postgres
+
+
 if [ "${MYVER}" != "5.6" ]
 then
 	${here}/install_mysql.sh
@@ -10,12 +12,19 @@ sudo cp -f ${here}/my${MYVER}.cnf /etc/mysql/conf.d/my.cnf
 sudo service mysql restart
 sudo cat /var/log/mysql/error.log
 
+mkdir ${here}/sakila
+cd ${here}/sakila
+wget http://downloads.mysql.com/docs/sakila-db.tar.gz
+tar xfz sakila-db.tar.gz
+
+sudo mysql -u root < ${here}/sakila/sakila-db/sakila-schema.sql
+sudo mysql -u root < ${here}/sakila/sakila-db/sakila-data.sql
+
 if [ "${MYVER}" != "5.6" ]
 then
 	sudo mysql -u root < ${here}/setup_mysql.sql
 else
 	mysql -u root < ${here}/setup_mysql.sql
 fi
-
 
 
