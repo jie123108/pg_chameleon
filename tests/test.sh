d4bf3a17f7a9733b81946013ca7919292239b685
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-07-28 21:35:14 +0100

    fix wrong connstring name

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,7 @@ set -e
 here=`dirname $0`
 chameleon.py set_config
 cp ${here}/test.yaml ~/.pg_chameleon/connection/ 
-chameleon.py create_service_schema --connfile test.yaml --connkey test
+chameleon.py create_service_schema --connfile test--connkey test
 chameleon.py add_replica --connfile test --connkey test
 chameleon.py init_replica --connfile test --connkey test
 chameleon.py show_status --connfile test --connkey test
