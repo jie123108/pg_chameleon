5800240bff9f2608be26f1d733f4a1d684f5eac3
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-07-28 16:57:47 +0300

    improve script

--- scripts/get_old_lib.sh
+++ scripts/get_old_lib.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/bin/bash 
+set -e 
+#set -x
+
 here=`dirname $0`
 old_lib="${here}/../old_lib"
 branch="ver1.6"
 files='global_lib sql_util pg_lib mysql_lib'
+if [ ! -d  "${old_lib}" ]
+then 
+	mkdir ${old_lib}
+fi
+
+
 for file in ${files}
 do
 git show ${branch}:pg_chameleon/lib/${file}.py   > ${old_lib}/${file}.py
 done 
-git show ${branch}:pg_chameleon/scripts/chameleon.py   > ${old_lib}/chameleon.py
+git show ${branch}:scripts/chameleon.py   > ${old_lib}/chameleon.py
