f16f3f0faac898ce68303b4d1c7e7f5276d1e32d
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-07-28 21:32:41 +0100

    update test file

--- tests/test.sh
+++ tests/test.sh
@@ -4,10 +4,10 @@ here=`dirname $0`
 chameleon.py set_config
 cp ${here}/test.yaml ~/.pg_chameleon/connection/ 
 chameleon.py create_service_schema --connfile test.yaml --connkey test
-chameleon.py add_replica --connfile test.yaml --connkey test
-chameleon.py init_replica --connfile test.yaml --connkey test
-chameleon.py show_status --connfile test.yaml --connkey test
-chameleon.py drop_replica --connfile test.yaml --connkey test --noprompt
-chameleon.py drop_service_schema --connfile test.yaml --connkey test --noprompt
+chameleon.py add_replica --connfile test --connkey test
+chameleon.py init_replica --connfile test --connkey test
+chameleon.py show_status --connfile test --connkey test
+chameleon.py drop_replica --connfile test --connkey test --noprompt
+chameleon.py drop_service_schema --connfile test --connkey test --noprompt
 
 
